35867bc27cf8ddb10802d0bec0e48c3ecb9ed5d3
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2026-06-25 13:21:08 -0400

    Create compute_sensitivities function in sensitivities.py
    Added a stub for computing sensitivities with logging.

---
+++ src/risk/sensitivities.py
@@ -0,0 +1,9 @@
+from ..utils.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+def compute_sensitivities():
+    logger.info("Computing sensitivities (stub)")
+    # TODO: implement ΔLMP, ΔLoad, ΔFuel, ΔConstraint
+    return {}
